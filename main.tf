c9a57fba95868549075d98dfa0403332a813066f
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-02-28 18:19:49 +0530

    sample

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name = "test.test1"
   value = "Hello world"
-  type = "securestring"
+  type = "SecureString"
   key_id = "23fbbd89-e1ad-4fe9-97d2-238a2f996ef9"
 }
