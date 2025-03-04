60fe2bcae66e3f7372a1b24cfb16525dbb18a4ff
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-03-04 17:04:54 +0530

    sample

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -25,3 +25,9 @@ parameters = [
   { name = "dev.payment.amqp_pass", value = "roboshop123" },
   { name = "dev.payment.amqp_user", value = "roboshop" }
 ]
+
+passwords = [
+  { name = "dev.payment.amqp_pass", value = "roboshop123" },
+  { name = "dev.shipping.db_pass", value = "RoboShop@1" }
+]
+
--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ resource "aws_ssm_parameter" "parameters" {
   type = "String"
   key_id= "23fbbd89-e1ad-4fe9-97d2-238a2f996ef9"
 }
+
+##Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
+
+resource "aws_ssm_parameter" "parameters" {
+  count = length (var.passwords)
+  name = var.passwords [count.index].name
+  value = var.passwords [count.index].value
+  type = "SecureString"
+  key_id= "23fbbd89-e1ad-4fe9-97d2-238a2f996ef9"
+}
