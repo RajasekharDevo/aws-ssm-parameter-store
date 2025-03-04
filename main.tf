ce053f1764e1b7c5b4b1a3227f22a3aadfb05d08
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-03-04 18:04:03 +0530

    sample

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_ssm_parameter" "parameters" {
   value = var.parameters [count.index].value
   type = "String"
   key_id= "23fbbd89-e1ad-4fe9-97d2-238a2f996ef9"
+  overwrite = true
 }
 
 ##Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
@@ -13,4 +14,5 @@ resource "aws_ssm_parameter" "passwords" {
   value = var.passwords [count.index].value
   type = "SecureString"
   key_id= "23fbbd89-e1ad-4fe9-97d2-238a2f996ef9"
+  overwrite = true
 }
