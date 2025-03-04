c4ec7ca91c2db8e5afea582b98ecabbdcd164104
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-03-04 17:17:02 +0530

    sample

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_ssm_parameter" "parameters" {
 
 ##Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
 
-resource "aws_ssm_parameter" "parameters" {
+resource "aws_ssm_parameter" "passwords" {
   count = length (var.passwords)
   name = var.passwords [count.index].name
   value = var.passwords [count.index].value
