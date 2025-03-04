e46afdd6df0fb57aa8638cd2ed59b1bb6828e391
RajasekharDevo <kotharajasekhar.qa@gmail.com>
2025-03-04 18:06:26 +0530

    sample

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ resource "aws_ssm_parameter" "parameters" {
   value = var.parameters [count.index].value
   type = "String"
   key_id= "23fbbd89-e1ad-4fe9-97d2-238a2f996ef9"
-  overwrite = true
 }
 
 ##Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
